955dd86d20df3f0b3878b7c6daad86a6f471e4ca
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-11-17 16:44:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
-    return 'hello flask & code deploy V2  good test !!'
+    return 'seo - hello flask & code deploy V2  good test !!'
 
 
 if __name__ == '__main__':
