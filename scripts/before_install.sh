759e12af5a167b9442a1dd66bc489d06688bd63c
JongHun_Seo <76559811+seojonghon@users.noreply.github.com>
2023-11-17 16:01:59 +0900

    Update before_install.sh
    배포시 이전 플라스크 서버 삭제

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+var=$(ps -ef | grep 'python3 -u app.py' | grep -v 'grep')
+pid=$(echo ${var} | cut -d " " -f2)
+if [ -n "${pid}" ]
+then 
+   kill -9 ${pid}
+   echo ${pid} is terminated.
+else
+   echo ${pid} is not running.
+fi
 
 rm -rf /home/ubuntu/hello-flask
 mkdir  /home/ubuntu/hello-flask
+
